5ffbfeda5eed961460904ef456cd102dcc1e3f0c
Adam Gatt <adam.gatt@baesystems.com>
2022-12-01 20:12:48 +1030

    Initial commit

---
+++ solution1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <numeric>
+
+#include "read_utils.h"
+
+int main() {
+    auto lines = ReadUtils::lines("inputs/input1.txt");
+
+    std::vector<int> largestThree(3, 0);
+
+    int currentTotal = 0;
+    for (const auto& line : lines) {
+        if (!line.empty()) {
+            currentTotal += std::stoi(line);
+        } else {
+            auto position = std::lower_bound(largestThree.cbegin(), largestThree.cend(), currentTotal);
+            if (position != largestThree.cbegin()) {
+                largestThree.insert(position, currentTotal);
+                largestThree.erase(largestThree.cbegin());
+            }
+            currentTotal = 0;
+        }
+    }
+
+    int answerA = *std::prev(largestThree.cend());
+    int answerB = std::accumulate(largestThree.cbegin(), largestThree.cend(), 0);
+
+    std::cout << answerA << " " << answerB << std::endl;
+
+    return 0; 
+}
