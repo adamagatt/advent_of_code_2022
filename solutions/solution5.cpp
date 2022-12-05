bfc26e372c40984ca28e122a8642b37665c61bf6
Adam Gatt <adam.gatt@live.com.au>
2022-12-05 21:34:36 +1030

    Most of program logic

--- solutions/solution5.cpp
+++ solutions/solution5.cpp
@@ -2,9 +2,11 @@
 
 #include "../utils/read.h"
 
+#include <algorithm>
 #include <array>
+#include <cctype>
 #include <deque>
-#include <iterator>
+#include <sstream>
 #include <utility>
 
 using Stack = std::deque<char>;
@@ -16,14 +18,44 @@ struct Instruction {
     int dest;
 };
 
+auto nextInteger(std::stringstream& ss) -> int {
+    int i;
+
+    while (!std::isdigit(ss.peek())) {
+        ss.ignore(1);
+    }
+    ss >> i;
+    return i;
+}
+
 auto parseInput(std::vector<std::string> lines) -> std::pair<Stacks, std::vector<Instruction>> {
     Stacks initialStacks;
     std::vector<Instruction> instructions;
 
     auto curLine = lines.cbegin();
-    // while (curLine->con)
+    // Initial stack info has at least one '[' per line
+    for ( ; curLine->find('[') != std::string::npos; ++curLine) {
+        for (int i = 0; i < 9; i++) {
+            if (char foundChar = curLine->at(i*4+1); foundChar != ' ') {
+                initialStacks[i].push_back(foundChar);
+            }
+        }
+    }
 
- 
+    // Skip the next two lines to get to instructions
+    std::advance(curLine, 2);
+
+    // Read instructions from here
+    for ( ; curLine != lines.cend(); ++curLine) {
+        int amount{}, source{}, dest{};
+        std::stringstream ss(*curLine);
+        amount = nextInteger(ss);
+        source = nextInteger(ss);
+        dest = nextInteger(ss);
+
+        instructions.emplace_back(amount, source, dest);
+    }
+
     return {initialStacks, instructions};
 }
 
@@ -31,11 +63,18 @@ auto executeInstruction(Stacks& stacks, const Instruction& instruction) {
     Stack& sourceStack = stacks[instruction.source - 1];
     Stack& destStack = stacks[instruction.dest - 1];
 
+    // Reverse source iterators to copy elements across in reverse order.
+    // Simulates the reversing effect of moving crates one-by-one.
     destStack.insert(
         destStack.cend(),
-        std::move_iterator(sourceStack.rbegin()),
-        std::move_iterator(std::next(sourceStack.rbegin(), instruction.amount))
+        sourceStack.crbegin(),
+        std::next(sourceStack.crbegin(), instruction.amount)
     );
+
+    // Would use deque::erase but apparently doesn't support reverse iterators
+    for (int i = 0; i < instruction.amount; ++i) {
+        sourceStack.pop_back();
+    }
 }
 
 auto Solutions::solution5() -> Answers {
@@ -45,5 +84,10 @@ auto Solutions::solution5() -> Answers {
         executeInstruction(stacks, instruction);
     }
 
-    return {"", ""};
+    std::string answerA{};
+    for (const Stack& stack : stacks) {
+        answerA.push_back(stack.back());
+    }
+
+    return {answerA, ""};
 }
