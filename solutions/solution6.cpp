eeca23ef36037605169eafd6f182682b7f492617
Adam Gatt <adam.gatt@live.com.au>
2022-12-06 19:20:03 +1030

    Solution 6 part B

--- solutions/solution6.cpp
+++ solutions/solution6.cpp
@@ -2,24 +2,63 @@
 
 #include "../utils/read.h"
 
+#include <algorithm>
 #include <string>
+#include <unordered_map>
 
-auto nextFourAllDifferent(const auto& it) -> bool {
-    char pos1 = *it;
-    char pos2 = *(std::next(it, 1));
-    char pos3 = *(std::next(it, 2));
-    char pos4 = *(std::next(it, 3));
+using Counter = std::unordered_map<char, int>;
 
-    return pos1 != pos2 && pos1 != pos3 && pos1 != pos4
-        && pos2 != pos3 && pos2 != pos4
-        && pos3 != pos4;
+auto anyRepeatedChars(const Counter& counter) -> bool {
+    return std::ranges::any_of(
+        counter,
+        [](const auto& keyValue) {
+            return keyValue.second > 1;
+        }
+    );
+}
+
+auto buildCounter(const auto& trailingIt, const auto& leadingIt) -> Counter {
+    Counter output;
+
+    for (auto it = trailingIt; it != leadingIt; ++it) {
+        char c = *it;
+        output[c] += 1;
+    }
+    // leading iterator is included (not past range like end iterators)
+    ++output[*leadingIt];
+
+    return output;
+}
+
+auto updateCounter(Counter& counter, const auto& trailingIt, const auto& leadingIt ) {
+    // Add the char ABOUT to enter the window
+    char addChar = *std::next(leadingIt);
+    ++counter[addChar];
+    
+    // Remove the char JUST leaving the window
+    char removeChar = *trailingIt;
+    --counter[removeChar];
+}
+
+auto answerForWindowSize(const std::string& input, const int windowSize) -> int {
+    auto trailingIt = input.cbegin();
+    // leading is end-of-window, not one past like end iterators
+    auto leadingIt = std::next(trailingIt, windowSize - 1);
+    Counter counter = buildCounter(trailingIt, leadingIt);
+
+    int answer = windowSize; // Marker position is counted from end of window
+    for ( ; anyRepeatedChars(counter); ++trailingIt, ++leadingIt, ++answer) {
+        updateCounter(counter, trailingIt, leadingIt);
+    }
+
+    return answer;
 }
 
 auto Solutions::solution6() -> Answers {
     std::string input = Utils::readLines("inputs/input6.txt").at(0);
 
-    int answerA = 4; // Marker position is counted from end of run of 4
-    for (auto it = input.cbegin(); !nextFourAllDifferent(it); ++it, ++answerA) { }
+    int answerA = answerForWindowSize(input, 4);
+    int answerB = answerForWindowSize(input, 14);
 
-    return { std::to_string(answerA), "" };
+    return { std::to_string(answerA), std::to_string(answerB) };
 }
