01cdbbe6956eed73a725ab3ddbcee3d5c55d4c21
Adam Gatt <adam.gatt@live.com.au>
2022-12-20 17:46:26 +1030

    Comment and cleanup

--- solutions/solution1.cpp
+++ solutions/solution1.cpp
@@ -1,7 +1,7 @@
 #include "solutions.h"
 
 #include <algorithm>
-#include <deque>
+#include <list>
 #include <numeric>
 
 #include "../utils/read.h"
@@ -9,23 +9,27 @@
 auto Solutions::solution1() -> Answers {
     auto lines = Utils::readLines("inputs/input1.txt");
 
-    std::deque<int> largestThree(3, 0);
+    std::list largestThree(3, 0);
 
-    int currentTotal = 0;
-    for (const auto& line : lines) {
+    for (int currentTotal = 0; const auto& line : lines) {
         if (!line.empty()) {
             currentTotal += std::stoi(line);
         } else {
+            // We have a completed total. Find out where it belongs in our sorted top-three list,
+            // insert it in the right position (found by lower_bound) if it qualifies. 
             auto position = std::ranges::lower_bound(largestThree, currentTotal);
             if (position != largestThree.cbegin()) {
                 largestThree.insert(position, currentTotal);
                 largestThree.pop_front();
             }
+
+            // New group will start, reset total
             currentTotal = 0;
         }
     }
 
-    int answerA = *std::prev(largestThree.cend());
+    // Last element in top-three list is largest
+    int answerA = *largestThree.crbegin();
     int answerB = std::accumulate(largestThree.cbegin(), largestThree.cend(), 0);
 
     return {std::to_string(answerA), std::to_string(answerB)};
